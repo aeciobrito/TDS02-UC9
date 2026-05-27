e9190f3f642155b8d0e82b4c526ea201c77819eb
AECIO MENDES BRITO <aeciobrito@gmail.com>
2026-05-26 21:38:35 -0300

    Atualiza a página de produtos para exibir a lista de produtos carregada da API

---
+++ 01/js/produtos/list.js
@@ -0,0 +1,27 @@
+async function carregarProdutos() {
+    try {
+        const resposnse = await fetch(`${API_BASE_URL}/produtos`);
+        const produtos = await resposnse.json();
+        console.log(produtos);
+
+        const tbody = document.getElementById("tabela-produtos");
+        tbody.innerHTML = '';
+
+        produtos.forEach(produto => {
+            const tr = document.createElement('tr');
+            tr.innerHTML = `
+                <td>${produto.id}</td>
+                <td>${produto.nome}</td>
+                <td>${produto.preco}</td>
+                <td>${produto.quantidadeEstoque}</td>
+                <td>${produto.fornecedorId}</td>
+            `      
+            tbody.appendChild(tr)   
+        });        
+
+    } catch (error) {
+        console.error("Erro ao carregar os produtos", error);
+    }
+}
+
+carregarProdutos();
